d19bef771673af104b2d07cd130bb62fbbdce49d
Apoorv Sharma <apoorv@pop-os.localdomain>
2023-12-04 21:39:49 +0530

    till day 3

---
+++ 2023/day2.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type game struct {
+	red        int
+	blue       int
+	green      int
+	gameNumber int
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	filepath := "inputs/day2.txt"
+	file, err := os.Open(filepath)
+
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	var file_lines []string
+
+	for scanner.Scan() {
+		file_lines = append(file_lines, scanner.Text())
+	}
+
+	var games []game
+
+	for _, line := range file_lines {
+		str := strings.Split(line, ":")
+
+		gameNumber, err := strconv.Atoi(strings.Split(str[0], " ")[1])
+
+		if err != nil {
+			panic(err)
+		}
+
+		game := game{red: 0, green: 0, blue: 0, gameNumber: gameNumber}
+
+		subsets := strings.Split(str[1], ";")
+
+		for _, subset := range subsets {
+			results := strings.Split(subset, ",")
+
+			for _, result := range results {
+				number, err := strconv.Atoi(strings.Split(result, " ")[1])
+
+				if err != nil {
+					panic(err)
+				}
+
+				color := strings.Split(result, " ")[2]
+
+				if color == "red" {
+					game.red = max(number, game.red)
+				}
+				if color == "blue" {
+					game.blue = max(number, game.blue)
+				}
+				if color == "green" {
+					game.green = max(number, game.green)
+				}
+			}
+
+		}
+
+		games = append(games, game)
+
+	}
+
+	sum := 0
+	power := 0
+
+	for _, game := range games {
+		red, blue, green, gameNumber := game.red, game.blue, game.green, game.gameNumber
+
+		if red <= 12 && blue <= 14 && green <= 13 {
+			sum = sum + gameNumber
+		}
+
+		multiple := red * blue * green
+
+		power = power + multiple
+	}
+
+	fmt.Println("Sum", sum, "Power", power)
+}
---
+++ 2023/day3.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+type indexValue struct {
+	number    string
+	end_index int
+}
+
+type position struct {
+	row    int
+	column int
+}
+
+type symbolPosition struct {
+	symbol   string
+	position position
+}
+
+type numberSymbol struct {
+	number   int
+	position position
+}
+
+func isStringNumber(letter string) bool {
+	string_numbers := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	value := false
+	for _, number := range string_numbers {
+		if letter == number {
+			value = true
+			break
+		}
+	}
+
+	return value
+}
+
+func getNumber(line string, index int, number string) indexValue {
+	if index < len(line) && isStringNumber(string(line[index])) {
+		number = number + string(line[index])
+		return getNumber(line, index+1, number)
+	} else {
+		value := indexValue{number, index - 1}
+
+		return value
+	}
+}
+
+func hasSymbolAdjacent(lines []string, line_index int, start_index int, end_index int) symbolPosition {
+	value := ""
+	str_position := position{0, 0}
+
+outer:
+	for i := max(line_index-1, 0); i <= min(line_index+1, len(lines)-1); i++ {
+		for j := start_index; j <= end_index; j++ {
+			letter := string(lines[i][j])
+			if letter == "." || isStringNumber(letter) {
+				continue
+			}
+			str_position = position{i, j}
+			value = letter
+			break outer
+
+		}
+	}
+
+	return symbolPosition{symbol: value, position: str_position}
+}
+
+func main() {
+	filepath := "inputs/day3.txt"
+	file, err := os.Open(filepath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	var file_lines []string
+
+	for scanner.Scan() {
+		file_lines = append(file_lines, scanner.Text())
+	}
+
+	sum := 0
+
+	gearRatio := 0
+
+	var star_numbers []numberSymbol
+
+	for line := 0; line < len(file_lines); line++ {
+		for index := 0; index < len(file_lines[line]); index++ {
+			letter := string(file_lines[line][index])
+
+			if letter == "." {
+				continue
+			}
+
+			if isStringNumber(string(file_lines[line][index])) {
+				start_index := index
+				str_number := letter
+				numberIndex := getNumber(file_lines[line], index+1, str_number)
+				end_index := numberIndex.end_index
+
+				str_number = numberIndex.number
+				index = end_index
+				number, err := strconv.Atoi(str_number)
+				if err != nil {
+					panic(err)
+				}
+				adjacent := hasSymbolAdjacent(file_lines, line, max(start_index-1, 0), min(end_index+1, len(file_lines[line])-1))
+				if adjacent.symbol != "" {
+					sum = sum + number
+				}
+
+				if adjacent.symbol == "*" {
+					exists := false
+					for i := 0; i < len(star_numbers); i++ {
+						if star_numbers[i].position == adjacent.position {
+							gearRatio = gearRatio + number*star_numbers[i].number
+							exists = true
+							break
+						}
+					}
+					if exists == false {
+						star_numbers = append(star_numbers, numberSymbol{number, adjacent.position})
+					}
+				}
+
+			}
+
+		}
+
+	}
+
+	fmt.Println(sum, gearRatio)
+}
